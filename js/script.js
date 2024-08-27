17c7b4b294adff89d4ed2891bc8060922f7eced5
Autumn60 <harada.akiro@gmail.com>
2024-08-27 22:56:11 +0900

    upload
    Signed-off-by: Autumn60 <harada.akiro@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(function() {
+  // Scroll
+  var navHeight = $('.header').outerHeight();
+
+  $('a[href^="#"]').on('click', function() {
+    var href = $(this).attr('href');
+    var target = $(href == '#' || href == '' ? 'html' : href);
+    var position = target.offset().top - navHeight;
+    $('html, body')
+        .animate(
+            {
+              scrollTop: position,
+            },
+            300, 'swing');
+    return false;
+  });
+
+  // Page Top
+  $('#js-page-top').on('click', function() {
+    $('body,html')
+        .animate(
+            {
+              scrollTop: 0,
+            },
+            300);
+    return false;
+  });
+});
